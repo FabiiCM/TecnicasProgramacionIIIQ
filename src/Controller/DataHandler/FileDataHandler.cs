dbf7e18acd38f1d89d7459c58854f28e5e9f0e7e
akanneee123 <akane26allen@gmail.com>
2024-11-12 03:38:46 +0100

    Implementación de los resgistros

---
+++ src/Controller/DataHandler/FileDataHandler.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Controller.DataHandler
+{
+
+    /// <summary>
+    /// Implementación de <see cref="IDataHandler"/> al usar archivos
+    /// </summary>
+    /// <see cref="Controller.DataHandler.IDataHandler"/>
+    internal class FileDataHandler : IDataHandler
+    {
+        /// <summary>
+        /// lee el dato de un file
+        /// </summary>
+        public void ReadData()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "Assets", "clientes.xlx");
+            var fileString = File.ReadAllText(path);
+            
+
+        }
+
+        /// <summary>
+        /// Escribe el dato de un file
+        /// </summary>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public void WriteData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Controller/DataHandler/IDataHandler.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Controller.DataHandler
+{
+
+    /// <summary>
+    //coctato para elementos que manejan datos
+    /// </summary>
+    public interface IDataHandler
+    {
+        /// <summary>
+        //Lee los archivos 
+        /// </summary>
+        public void ReadData();
+
+        /// <summary>
+        //escribe los archivos
+        /// </summary>
+        public void WriteData();
+
+    }
+}
