f7093efbcbc3088ac37ab992d7a821ba1b96f3bc
FabiiCM <fcastrom@edu.upolitecnica.cr>
2024-11-21 13:47:02 -0600

    FORMEntrenador Menú entrenador implementar entrenador al iniciar sesion Interfaz

---
+++ src/ProyectoGym/ProyectoGym/FRMEntrenador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoGym
+{
+    public partial class FRMEntrenador : Form
+    {
+        public FRMEntrenador()
+        {
+            InitializeComponent();
+        }
+
+        private void BTNSalir_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            FRMInicio ventanaSesion = new FRMInicio();
+            ventanaSesion.Show();
+        }
+    }
+}
