62d79c5a2d4bb8be96faf34bff0de8f21e4319b0
akanneee123 <akane26allen@gmail.com>
2024-12-06 10:08:05 -0600

    Eliminación de codigo duplicado

--- src/ProyectoGym/ProyectoGym/FRMInventario.Designer.cs
+++ src/ProyectoGym/ProyectoGym/FRMInventario.Designer.cs
@@ -44,17 +44,19 @@
             // 
             // TXBNombre
             // 
-            TXBNombre.Location = new Point(37, 36);
+            TXBNombre.Location = new Point(26, 22);
+            TXBNombre.Margin = new Padding(2, 2, 2, 2);
             TXBNombre.Name = "TXBNombre";
-            TXBNombre.Size = new Size(150, 31);
+            TXBNombre.Size = new Size(106, 23);
             TXBNombre.TabIndex = 0;
             TXBNombre.Text = "Nombre";
             // 
             // TXBCantidad
             // 
-            TXBCantidad.Location = new Point(205, 38);
+            TXBCantidad.Location = new Point(144, 23);
+            TXBCantidad.Margin = new Padding(2, 2, 2, 2);
             TXBCantidad.Name = "TXBCantidad";
-            TXBCantidad.Size = new Size(150, 31);
+            TXBCantidad.Size = new Size(106, 23);
             TXBCantidad.TabIndex = 1;
             TXBCantidad.Text = "Cantidad";
             // 
@@ -62,9 +64,10 @@
             // 
             CMBEstado.FormattingEnabled = true;
             CMBEstado.Items.AddRange(new object[] { "Nuevo", "Usado", "Malo" });
-            CMBEstado.Location = new Point(378, 36);
+            CMBEstado.Location = new Point(265, 22);
+            CMBEstado.Margin = new Padding(2, 2, 2, 2);
             CMBEstado.Name = "CMBEstado";
-            CMBEstado.Size = new Size(168, 33);
+            CMBEstado.Size = new Size(119, 23);
             CMBEstado.TabIndex = 2;
             CMBEstado.Text = "Estado";
             // 
@@ -75,9 +78,10 @@
             BTAgregar.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 224, 192);
             BTAgregar.FlatStyle = FlatStyle.Flat;
             BTAgregar.ForeColor = Color.FromArgb(255, 128, 0);
-            BTAgregar.Location = new Point(37, 216);
+            BTAgregar.Location = new Point(26, 130);
+            BTAgregar.Margin = new Padding(2, 2, 2, 2);
             BTAgregar.Name = "BTAgregar";
-            BTAgregar.Size = new Size(112, 34);
+            BTAgregar.Size = new Size(78, 33);
             BTAgregar.TabIndex = 3;
             BTAgregar.Text = "Agregar";
             BTAgregar.UseVisualStyleBackColor = false;
@@ -90,9 +94,10 @@
             BTActualizar.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 224, 192);
             BTActualizar.FlatStyle = FlatStyle.Flat;
             BTActualizar.ForeColor = Color.FromArgb(255, 128, 0);
-            BTActualizar.Location = new Point(243, 216);
+            BTActualizar.Location = new Point(170, 130);
+            BTActualizar.Margin = new Padding(2, 2, 2, 2);
             BTActualizar.Name = "BTActualizar";
-            BTActualizar.Size = new Size(112, 34);
+            BTActualizar.Size = new Size(78, 33);
             BTActualizar.TabIndex = 4;
             BTActualizar.Text = "Actualizar";
             BTActualizar.UseVisualStyleBackColor = false;
@@ -104,9 +109,10 @@
             BTNEliminar.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 224, 192);
             BTNEliminar.FlatStyle = FlatStyle.Flat;
             BTNEliminar.ForeColor = Color.FromArgb(255, 128, 0);
-            BTNEliminar.Location = new Point(434, 216);
+            BTNEliminar.Location = new Point(304, 130);
+            BTNEliminar.Margin = new Padding(2, 2, 2, 2);
             BTNEliminar.Name = "BTNEliminar";
-            BTNEliminar.Size = new Size(112, 34);
+            BTNEliminar.Size = new Size(78, 33);
             BTNEliminar.TabIndex = 5;
             BTNEliminar.Text = "Eliminar";
             BTNEliminar.UseVisualStyleBackColor = false;
@@ -116,10 +122,11 @@
             dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Nombre, Cantidad, Estado });
-            dataGridView1.Location = new Point(37, 88);
+            dataGridView1.Location = new Point(26, 53);
+            dataGridView1.Margin = new Padding(2, 2, 2, 2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(509, 94);
+            dataGridView1.Size = new Size(356, 56);
             dataGridView1.TabIndex = 6;
             // 
             // Nombre
@@ -150,9 +157,10 @@
             BTNCerrar.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 224, 192);
             BTNCerrar.FlatStyle = FlatStyle.Flat;
             BTNCerrar.ForeColor = Color.FromArgb(255, 128, 0);
-            BTNCerrar.Location = new Point(462, 304);
+            BTNCerrar.Location = new Point(314, 185);
+            BTNCerrar.Margin = new Padding(2, 2, 2, 2);
             BTNCerrar.Name = "BTNCerrar";
-            BTNCerrar.Size = new Size(84, 35);
+            BTNCerrar.Size = new Size(59, 27);
             BTNCerrar.TabIndex = 7;
             BTNCerrar.Text = "Cerrar";
             BTNCerrar.UseVisualStyleBackColor = false;
@@ -160,10 +168,10 @@
             // 
             // FRMInventario
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Window;
-            ClientSize = new Size(602, 351);
+            ClientSize = new Size(421, 243);
             Controls.Add(BTNCerrar);
             Controls.Add(dataGridView1);
             Controls.Add(BTNEliminar);
@@ -172,6 +180,7 @@
             Controls.Add(CMBEstado);
             Controls.Add(TXBCantidad);
             Controls.Add(TXBNombre);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "FRMInventario";
             Text = "FRMInventario";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
