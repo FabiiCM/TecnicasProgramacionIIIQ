79232a457f96c86ebd45da01b9c66c080813522a
FabiiCM <fcastrom@edu.upolitecnica.cr>
2024-12-12 23:52:00 -0600

    Migrations

--- ProyectoGym/Migrations/20241205152328_primeras-tablas.Designer.cs
+++ ProyectoGym/Migrations/20241205152328_primeras-tablas.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using src.Model.Context;
+using ProyectoGym.src.Model.Context;
 
 #nullable disable
 
