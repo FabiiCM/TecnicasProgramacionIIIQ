f123ca7793ca8592e0a5dc6403947e1d9caf6b79
FabiiCM <fcastrom@edu.upolitecnica.cr>
2024-12-13 00:53:31 -0600

    Requerido

--- proyectoGym/src/Model/Context/GymContext.cs
+++ proyectoGym/src/Model/Context/GymContext.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace src.Model.Context
+namespace ProyectoGym.src.Model.Context
 {
     public class GymContext : DbContext
     {
