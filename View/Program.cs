24fcbee487282d9e908689771fecaf534644eb3a
FabiiCM <fcastrom@edu.upolitecnica.cr>
2024-12-12 22:49:29 -0600

    Implementación de Blazer (View)

---
+++ View/Program.cs
@@ -0,0 +1,34 @@
+using View.Components;
+using ProyectoGym.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<GymContext>(opciones =>
+{
+    opciones.UseSqlServer("Server=DESKTOP-NUJHIPK;Database=GymDB;Trusted_Connection=True;TrustServerCertificate=True");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
