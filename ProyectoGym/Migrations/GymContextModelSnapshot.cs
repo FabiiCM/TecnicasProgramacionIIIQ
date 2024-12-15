6a2cfdc0226d48aec6f94e4a75fcf7cbbb8971a0
FabiiCM <fcastrom@edu.upolitecnica.cr>
2024-12-15 02:14:35 -0600

    Se eliminaron FORMS por errores en compilación / Implementar en Migrations conexión / posiblemente pensamos lograr implementar los FORMS como desde el inicio / El programa compila bien y con 0 errores

--- ProyectoGym/Migrations/GymContextModelSnapshot.cs
+++ ProyectoGym/Migrations/GymContextModelSnapshot.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using src.Model.Context;
+using ProyectoGym.src.Model.Context;
 
 #nullable disable
 
