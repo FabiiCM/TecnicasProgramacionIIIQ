130996bf1e7ea0fd1824a634a9c3778b951934c9
FabiiCM <fcastrom@edu.upolitecnica.cr>
2024-12-16 00:38:43 -0600

    Implementación de migrations / cambios View

--- View/Program.cs
+++ View/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddDbContext<GymContext>(opciones =>
 {
-    opciones.UseSqlServer("Server=DESKTOP-NUJHIPK;Database=GymDB;Trusted_Connection=True;TrustServerCertificate=True");
+    opciones.UseSqlServer("Server=DESKTOP-6EQ9TGO;Database=GymDB;Trusted_Connection=True;TrustServerCertificate=True");
 });
 
 builder.Services.AddQuickGridEntityFrameworkAdapter();
