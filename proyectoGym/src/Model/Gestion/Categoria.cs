2b1951653c27d50d558ee450a998b8ece21eeb22
FabiiCM <fcastrom@edu.upolitecnica.cr>
2024-12-17 23:36:58 -0600

    Implementamos métricas / Categorías / Modelos

---
+++ proyectoGym/src/Model/Gestion/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using Model.Gestion;
+using src.Model.Personas;
+using System.ComponentModel.DataAnnotations;
+
+namespace src.Model.Gestion
+{
+    /// <summary>
+    /// Representa una rutina específica para cada cliente.
+    /// </summary>
+    public class Categoria
+    {
+        [Key]
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece el cliente a la rutina.
+        /// </summary>
+        public string Nombre { get; set; }
+    }
+}
