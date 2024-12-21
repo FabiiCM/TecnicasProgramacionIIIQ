f1b8729b1a1b3d69f421957b1cab760c05725b96
FabiiCM <fcastrom@edu.upolitecnica.cr>
2024-12-20 18:42:01 -0600

    Comentarios

--- View/Program.cs
+++ View/Program.cs
@@ -8,7 +8,7 @@ using src.Model.Gestion;
 using src.Model.Finanzas;
 using BlazorDownloadFile;
 using QuestPDF.Infrastructure;
-
+//(como cadenas de conexión, configuración, logging, etc.).
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
@@ -20,7 +20,7 @@ builder.Services.AddDbContext<GymContext>(opciones =>
 {
     opciones.UseSqlServer("Server=DESKTOP-6EQ9TGO;Database=GymDB;Trusted_Connection=True;TrustServerCertificate=True");
 });
-
+//es parte de la configuración de servicios en la aplicación
 builder.Services.AddQuickGridEntityFrameworkAdapter();
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -50,25 +50,27 @@ builder.Services.AddScoped<ReporteIngresosController>();
 builder.Services.AddScoped<ReporteClasesController>();
 
 
-
+//Indica que estás utilizando la versión gratuita de QuestPDF bajo la licencia "Community License"
+// que permite su uso en proyectos personales, educativos y de bajo presupuesto.
 QuestPDF.Settings.License = LicenseType.Community;
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Comprueba si la aplicación se está ejecutando en un entorno de desarrollo.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
     app.UseMigrationsEndPoint();
-}
+}   //permite que las migraciones de la base de datos (aplicadas mediante
+    //Entity Framework Core) sean ejecutadas en un entorno de producción.
 
 app.UseHttpsRedirection();
-
+//Habilita el acceso a archivos estáticos en la aplicación
 app.UseStaticFiles();
 app.UseAntiforgery();
-
+//Configura el mapeo de componentes Razor en la aplicación.
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
